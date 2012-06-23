3278ed2dea022d13cc01339770ffe1bf20eef45a
ital <exodusrasta@gmail.com>
2012-06-22 23:26:58 -0700

    Update Sitemap Controlle, Update Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Blow::Application.routes.draw do
  
-  get "sitemap/index"
-
   resources :posts
 
 root :to => "posts#index"
@@ -75,4 +73,6 @@ resources :users, :only => [:show, :index]
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  match "/sitemap" => "sitemap#index", :as => :sitemap, :defaults => {:format => :xml}
+
 end
