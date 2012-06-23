a7d8cda17623a000cdb91273427f119a0bb16a8b
ital <exodusrasta@gmail.com>
2012-06-23 13:02:19 -0700

    Install sitemap_generator Gem

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,8 +4,9 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     if user.has_role? :admin
-      can :manage, :all
-    end
+    else
+    can :read, :all
+  end
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
